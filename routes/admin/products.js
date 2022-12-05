4340cfbc3a96149d84a298d9d87e9e5de228a87c
thijsBoet <m.boet@chello.nl>
2022-12-05 16:12:56 +0100

    creating image uploud functionality

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,17 +1,33 @@
 const express = require('express');
+const multer = require('multer');
+const { validationResult } = require('express-validator');
+
 const productsRepo = require('../../repositories/products');
-const ProductsNewTemplate = require('../../views/admin/products/new');
-
-
+const productsNewTemplate = require('../../views/admin/products/new');
+const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get('/admin/products', (req, res) => {
     res.send('Products')
 });
 
 router.get('/admin/products/new', (req, res) => {
-    res.send(ProductsNewTemplate({}));
+    res.send(productsNewTemplate({}));
 });
+
+router.post(
+	'/admin/products/new',
+	[requireTitle, requirePrice],
+	upload.single('image'),
+	(req, res) => {
+		errors = validationResult(req);
+
+        req.file;
+
+		res.send('submitted');
+	}
+);
 
 module.exports = router
